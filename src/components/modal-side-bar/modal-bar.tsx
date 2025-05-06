53bea62b9d4b37760cbf0896dea6c377b6d3b08a
Яков Савельев <jakobs@Akovs-MacBook-Pro.local>
2025-05-06 19:56:53 +0200

    reusable-modal-component

--- src/components/modal-side-bar/modal-bar.tsx
+++ src/components/modal-side-bar/modal-bar.tsx
@@ -1,16 +1,11 @@
 import React, {useState} from 'react';
-import {
-  View,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  FlatList,
-  Modal,
-} from 'react-native';
+import {View, TouchableOpacity, FlatList} from 'react-native';
 import {SidebarIcon} from '../../assets/icons';
 import {ISidebarDropdownDataSet} from './modal-bar.types';
 import {sidebarModalSets} from './modal-bar.constants';
 import {TextNormal} from '../text-titles/text-styled';
 import {TManualEncryptionScreens} from '../../app/navigator/screens';
+import ModalPopup from '../modal-popup/modal-popup';
 
 export interface ISidebarProps {
   currentScreen: TManualEncryptionScreens;
@@ -18,10 +13,6 @@ export interface ISidebarProps {
 
 const DropdownButton: React.FC<ISidebarProps> = ({currentScreen}) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const handleOutsidePress = () => {
-    setIsDropdownOpen(false);
-  };
 
   const handleSidebarPress = () => {
     setIsDropdownOpen(prev => !prev);
@@ -31,45 +22,35 @@ const DropdownButton: React.FC<ISidebarProps> = ({currentScreen}) => {
     return null;
   }
 
+  const optionsListComponent = (
+    <FlatList
+      data={sidebarModalSets[currentScreen as keyof typeof sidebarModalSets]}
+      renderItem={({item}: {item: ISidebarDropdownDataSet}) => (
+        <TouchableOpacity
+          className="flex-row items-center mb-2"
+          onPress={() => {
+            item.action();
+            setIsDropdownOpen(false);
+          }}>
+          {item.icon}
+          <TextNormal className="text-white ml-2">{item.label}</TextNormal>
+        </TouchableOpacity>
+      )}
+      keyExtractor={item => item.id}
+    />
+  );
+
   return (
     <View className="relative">
       <TouchableOpacity onPress={handleSidebarPress}>
         <SidebarIcon />
       </TouchableOpacity>
-
-      <Modal
-        animationType="fade"
-        transparent
-        visible={isDropdownOpen}
-        onRequestClose={handleSidebarPress}>
-        <TouchableWithoutFeedback onPress={handleOutsidePress}>
-          <View className="flex-1 bg-transparent">
-            <View className="absolute top-32 left-4 bg-gray-900 p-3 rounded-lg w-48">
-              <FlatList
-                data={
-                  sidebarModalSets[
-                    currentScreen as keyof typeof sidebarModalSets
-                  ]
-                }
-                renderItem={({item}: {item: ISidebarDropdownDataSet}) => (
-                  <TouchableOpacity
-                    className="flex-row items-center mb-2"
-                    onPress={() => {
-                      item.action();
-                      setIsDropdownOpen(false);
-                    }}>
-                    {item.icon}
-                    <TextNormal className="text-white ml-2">
-                      {item.label}
-                    </TextNormal>
-                  </TouchableOpacity>
-                )}
-                keyExtractor={item => item.id}
-              />
-            </View>
-          </View>
-        </TouchableWithoutFeedback>
-      </Modal>
+      <ModalPopup
+        content={optionsListComponent}
+        wrapperClass="absolute top-32 left-4 bg-gray-900 p-3 rounded-lg w-48"
+        isOpen={isDropdownOpen}
+        setisModalOpen={setIsDropdownOpen}
+      />
     </View>
   );
 };
--- src/components/themed-button/themed-button.style.ts
+++ src/components/themed-button/themed-button.style.ts
@@ -3,7 +3,7 @@ export const styles = {
     'flex flex-row w-80 justify-center content-center items-center text-center rounded-full h-12',
   defaultButtonText: 'self-center font-medium text-base text-gray-500',
   lightButtonText: 'self-center font-medium text-base text-white',
-  filled: 'mr-0 text-white bg-[#CE8946]',
+  filled: 'mr-0 text-white bg-[#CE8946]', // TODO: #CE8946 to the theme color system
   light: 'mr-0 text-gray-500 bg-white',
   lightBordered: 'mr-0 text-gray-500 bg-white border-[1px] border-opacity-gray',
   buttonContainer: 'items-center',
